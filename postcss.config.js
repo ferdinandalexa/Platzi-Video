38942dab044865ccef8976e06c51f5d3e92566dc
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2019-04-10 13:07:19 -0500

    Min CSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,26 @@ module.exports = {
         // require('autoprefixer')({
         //     grid: true
         // }),
-        require('postcss-import')(),
+        require('postcss-import')({
+            plugins: [
+                require('stylelint')({
+                    "rules": {
+                        "block-no-empty": true,
+                        "unit-whitelist": ["em", "rem", "%", "s", "fr", "vh", "dpi", "px"]
+                    } 
+                })
+            ]
+        }),
+        require('postcss-color-hwb')(),
         require('postcss-preset-env')({
             stage: 1,
             autoprefixer: {
                 grid: true
             }
         }),
-        require('postcss-color-hwb')(),
+        require('css-mqpacker')(),
+        require("cssnano")({
+            autoprefixer: false,
+        }),
     ]
 }
